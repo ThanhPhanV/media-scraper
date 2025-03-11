e45b01ee1a3068ab00f1c95c52f461f328b597cb
Phan Thanh <pvthanh98it@gmail.com>
2025-03-11 21:57:26 +0700

    feat: font-end - media

--- front-end/src/hooks/auth-hook.tsx
+++ front-end/src/hooks/auth-hook.tsx
@@ -2,10 +2,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../redux/store";
 import { IUser } from "../interfaces/user.interface";
 import { setAuth } from "../redux/reducers/auth-reducer";
+import { useNavigate } from "react-router-dom";
 
 export function useAppAuth() {
   const { token, user } = useSelector((state: RootState) => state.auth);
   const dispatch = useDispatch();
+  const nav = useNavigate();
   const login = (token: string, user: IUser) => {
     localStorage.setItem("token", token);
     localStorage.setItem("user", JSON.stringify(user));
@@ -16,6 +18,7 @@ export function useAppAuth() {
     localStorage.removeItem("token");
     localStorage.removeItem("user");
     dispatch(setAuth({ token: null, user: null }));
+    nav("/sign-in");
   };
 
   return {
--- front-end/src/layouts/dashboard.layout.tsx
+++ front-end/src/layouts/dashboard.layout.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import { AppTitle } from "../components/app-title";
 import { HeaderTitle } from "../components/h1-header";
 import { AppMenu } from "../components/menu";
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
+import LogoutIcon from "@mui/icons-material/Logout";
+import { useAppAuth } from "../hooks/auth-hook";
+import { IconButton } from "@mui/material";
 
 export function DashboardLayout({
   children,
@@ -10,10 +15,19 @@ export function DashboardLayout({
   children: React.ReactNode;
   title?: string;
 }) {
+  const { user, logout } = useAppAuth();
   return (
     <div className="grid grid-cols-12 funnel-display-300 mx-auto">
       <div className="grid col-span-12">
-        <AppTitle />
+        <div className="flex items-center justify-between">
+          <AppTitle />
+          <div className="flex items-center px-5">
+            <div className="mr-2">{user?.userName} </div>
+            <IconButton onClick={logout}>
+              <LogoutIcon />
+            </IconButton>
+          </div>
+        </div>
       </div>
       <div className="grid col-span-3 mt-3">
         <div>
