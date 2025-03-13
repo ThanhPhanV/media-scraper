faf6bb200875bae0ebac15aafbcbc98716a93fa2
Phan Thanh <pvthanh98it@gmail.com>
2025-03-13 13:06:33 +0700

    feat: everything is done, just optimize

--- back-end-api/src/config/app.config.ts
+++ back-end-api/src/config/app.config.ts
@@ -1,3 +1,4 @@
 export const API_PREFIX = 'api';
 export const PAGINATION_CONFIG_DEFAULT_PAGE = 1;
 export const PAGINATION_CONFIG_DEFAULT_LIMIT = 10;
+export const URL_EXCLUDE_LOGGING = ['api/auth/sign-in'];
--- back-end-api/src/interceptor/response-transform.itct.ts
+++ back-end-api/src/interceptor/response-transform.itct.ts
@@ -6,6 +6,7 @@ import {
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { URL_EXCLUDE_LOGGING } from 'src/config/app.config';
 import { LoggerService } from 'src/modules/logger/logger.service';
 
 export interface Response<T> {
@@ -26,13 +27,15 @@ export class ResTransformInterceptor<T>
     const request = context.switchToHttp().getRequest();
     return next.handle().pipe(
       map((data) => {
-        this.loggerService.log({
-          url: request.url,
-          method: request.method,
-          body: JSON.stringify(request.body || ''),
-          status: response.statusCode,
-          res: JSON.stringify(data || ''),
-        });
+        if (!URL_EXCLUDE_LOGGING.includes(request.url)) {
+          this.loggerService.log({
+            url: request.url,
+            method: request.method,
+            body: JSON.stringify(request.body || ''),
+            status: response.statusCode,
+            res: JSON.stringify(data || ''),
+          });
+        }
         return { status: response.statusCode, data };
       }),
     );
